fc81893d0c15b0ba577707798e6257278c81189d
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-09-11 14:12:49 +0200

    Forms

--- contacts-angular/src/app/contacts/contact-create/contact-create.component.ts
+++ contacts-angular/src/app/contacts/contact-create/contact-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-create',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactCreateComponent implements OnInit {
 
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.email]),
+    phone: new FormControl(''),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get name() {
+    return this.form.get('name');
+  }
+
+  create() {
+    console.log('create');
+  }
 }
--- contacts-angular/src/app/shared/shared.module.ts
+++ contacts-angular/src/app/shared/shared.module.ts
@@ -1,28 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectComponent } from './select/select.component';
-import { MatButtonModule } from "@angular/material/button";
-import { MatListModule } from "@angular/material/list";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { KebabPipe } from './kebab.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    SelectComponent,
-    KebabPipe,
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [ // ces modules deviennent disponible dans les modules qui importent Shared (par convention => tous les autres modules)
+  declarations: [SelectComponent, KebabPipe],
+  imports: [CommonModule],
+  exports: [
+    // ces modules deviennent disponible dans les modules qui importent Shared (par convention => tous les autres modules)
     CommonModule,
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
     SelectComponent,
     MatListModule,
-    KebabPipe
-  ]
+    KebabPipe,
+    ReactiveFormsModule,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
